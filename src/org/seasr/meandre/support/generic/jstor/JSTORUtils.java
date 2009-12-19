eaee47d43ff2e5e30caa3e572caf6130eccfa0db
Boris Capitanu <capitanu@illinois.edu>
2009-12-19 06:29:21 +0000

    - updated to work with Zotero-created urls

--- src/org/seasr/meandre/support/generic/jstor/JSTORUtils.java
+++ src/org/seasr/meandre/support/generic/jstor/JSTORUtils.java
@@ -66,10 +66,15 @@ public abstract class JSTORUtils {
     /** Regular expression to extract the document URL from the JSTOR intermediary page */
     public static final Pattern JSTOR_REGEX;
 
+    /** Regular expression to extract the intermediary URL from the JSTOR Item View URL usually saved by Zotero */
+    public static final Pattern JSTOR_REGEX_ZOTERO;
+
     static {
         // the code expects group 2 to contain the actual URL
         // do not forget to adjust the code if you change the regular expression
-        JSTOR_REGEX = Pattern.compile(".*<a href=(\"|\')(.+)\\1>Proceed to PDF</a>.*");
+        JSTOR_REGEX = Pattern.compile(".*href=(\"|\')(.+)\\1>Proceed to PDF.*");
+
+        JSTOR_REGEX_ZOTERO = Pattern.compile(".*(/page/termsConfirm.+?\\.pdf).*");
     }
 
     /**
@@ -85,36 +90,28 @@ public abstract class JSTORUtils {
         int connectTimeout = jstorConnection.getConnectTimeout();
         int readTimeout = jstorConnection.getReadTimeout();
 
-        HttpClient client = new HttpClient();
-        HttpConnectionManagerParams connectionParams = client.getHttpConnectionManager().getParams();
-        connectionParams.setConnectionTimeout(connectTimeout);
-        connectionParams.setSoTimeout(readTimeout);
-        client.getParams().setCookiePolicy(CookiePolicy.RFC_2109);
+        String location = jstorConnection.getURL().toString();
+        String cookies;
 
-        String response = null;
+        do {
+            String[] data = getResponseAndCookies(location, connectTimeout, readTimeout);
+            cookies = data[1];
 
-        GetMethod getMethod = new GetMethod(jstorConnection.getURL().toString());
-        try {
-            client.executeMethod(getMethod);
-            response = IOUtils.getTextFromReader(
-                    new InputStreamReader(getMethod.getResponseBodyAsStream()));
-        }
-        finally {
-            getMethod.releaseConnection();
-        }
+            Matcher matcher = JSTOR_REGEX_ZOTERO.matcher(data[0]);
+            if (matcher.find()) {
+                location = "http://www.jstor.org" + matcher.group(1);
+                continue;
+            } else {
+                matcher = JSTOR_REGEX.matcher(data[0]);
+                if (matcher.find()) {
+                    location = "http://www.jstor.org" + matcher.group(2);
+                    break;
+                } else
+                    throw new JSTORException("Could not extract the document URL from the JSTOR response page");
+            }
+        } while (true);
 
-        StringBuffer sb = new StringBuffer();
-        for (Cookie cookie : client.getState().getCookies())
-            sb.append("; ").append(cookie.toExternalForm());
-
-        String cookies = sb.substring(2);
-
-        Matcher matcher = JSTOR_REGEX.matcher(response);
-        if (!matcher.find())
-            throw new JSTORException("Could not extract the document URL from the JSTOR response page");
-
-        String documentURL = "http://www.jstor.org" + matcher.group(2);
-        URL url = new URL(documentURL);
+        URL url = new URL(location);
 
         URLConnection connection = url.openConnection();
         connection.setConnectTimeout(connectTimeout);
@@ -146,5 +143,42 @@ public abstract class JSTORUtils {
      */
     public static URLConnection getURLConnection(String jstorURL) throws JSTORException, IOException {
         return getURLConnection(new URL(jstorURL));
+    }
+
+    /**
+     * Helper method that reads the content from a URL and extracts the cookies
+     *
+     * @param url The URL
+     * @param connectTimeout Connection timeout
+     * @param readTimeout Read timeout
+     * @return An array with response at position 0 and cookies at position 1
+     * @throws IOException Error occurred
+     */
+    private static String[] getResponseAndCookies(String url, int connectTimeout, int readTimeout) throws IOException {
+        HttpClient client = new HttpClient();
+        HttpConnectionManagerParams connectionParams = client.getHttpConnectionManager().getParams();
+        connectionParams.setConnectionTimeout(connectTimeout);
+        connectionParams.setSoTimeout(readTimeout);
+        client.getParams().setCookiePolicy(CookiePolicy.RFC_2109);
+
+        String response = null;
+
+        GetMethod getMethod = new GetMethod(url);
+        try {
+            client.executeMethod(getMethod);
+            response = IOUtils.getTextFromReader(
+                    new InputStreamReader(getMethod.getResponseBodyAsStream()));
+        }
+        finally {
+            getMethod.releaseConnection();
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (Cookie cookie : client.getState().getCookies())
+            sb.append("; ").append(cookie.toExternalForm());
+
+        String cookies = sb.substring(2);
+
+        return new String[] { response, cookies };
     }
 }
